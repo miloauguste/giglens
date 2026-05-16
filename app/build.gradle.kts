59ed091d35503fd37c19ba2db77868b288b16584
Milo Auguste Jr <miloauguste@gmail.com>
2026-05-15 23:00:32 -0400

    docs: update session summary May 15 2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.augusteenterprise.giglens"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
     }
 
     buildTypes {
