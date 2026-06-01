9b4da3f3efe8d68ceb6ced8c0e14ae2fd52de822
Milo Auguste Jr <miloauguste@gmail.com>
2026-06-01 19:20:09 -0400

    Chore: remove sensitive files from tracking, update gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.augusteenterprise.giglens"
         minSdk = 26
         targetSdk = 35
-        versionCode = 68
-        versionName = "0.1.66"
+        versionCode = 69
+        versionName = "0.1.67"
     }
 
     signingConfigs {
--- key.sh
+++
@@ -1,3 +0,0 @@
-export GIGLENS_STORE_PASSWORD='hkQgLpRpptK1wGDxi5m7' && \
-export GIGLENS_KEY_PASSWORD='hkQgLpRpptK1wGDxi5m7' && \
-cd /home/poppa/giglens && ./gradlew bundleRelease 2>&1 | tail -10
